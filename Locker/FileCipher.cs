ed63c99cf270d454627908a5adac477f4db55dbc
Eduardo Neves Córdova <eduucordova@gmail.com>
2018-03-14 22:44:26 -0300

    code

---
+++ Locker/FileCipher.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Locker
+{
+    public class FileCipher
+    {
+        public void EncryptFiles(FileInfo[] files, string outputDirectory, string password)
+        {
+            foreach (var fileInfos in files)
+            {
+                FileEncrypt(fileInfos.FullName, outputDirectory, password);
+            }
+        }
+
+        public void DecryptFiles(IEnumerable<FileInfo> filesInfo, string outputDirectory, string password)
+        {
+            foreach (var fileInfo in filesInfo)
+            {
+                FileDecrypt(fileInfo.FullName, outputDirectory + "\\" + $"D_{DateTime.Now.ToString("yyyyMMddHHmmss")}", password);
+            }
+        }
+
+        public string Hash(string password)
+        {
+            byte[] salt = GenerateRandomSalt();
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            
+            byte[] hashBytes = new byte[52];
+            Array.Copy(salt, 0, hashBytes, 0, 32);
+            Array.Copy(hash, 0, hashBytes, 32, 20);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+        
+        public bool Verify(string password, string savedPasswordHash)
+        {
+            byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+
+            byte[] salt = new byte[32];
+            Array.Copy(hashBytes, 0, salt, 0, 32);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            for (int i = 0; i < 20; i++)
+                if (hashBytes[i + 32] != hash[i])
+                    throw new UnauthorizedAccessException();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Creates a random salt that will be used to encrypt your file. This method is required on FileEncrypt.
+        /// </summary>
+        /// <returns></returns>
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fille the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Encrypts a file from its path and a plain password.
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="password"></param>
+        private void FileEncrypt(string inputFile, string outputDirectory, string password)
+        {
+            var outputFile = Path.Combine(outputDirectory, Guid.NewGuid().ToString("N"));
+
+            byte[] salt = GenerateRandomSalt();
+            
+            FileStream fsCrypt = new FileStream(outputFile, FileMode.Create);
+            
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            
+            RijndaelManaged AES = new RijndaelManaged
+            {
+                KeySize = 256,
+                BlockSize = 128,
+                Mode = CipherMode.CBC
+            };
+            
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    cs.Write(buffer, 0, read);
+                }
+                cs.FlushFinalBlock();
+                // Close up
+                fsIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                cs.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        /// <summary>
+        /// Decrypts an encrypted file with the FileEncrypt method through its path and the plain password.
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="outputFile"></param>
+        /// <param name="password"></param>
+        private void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged
+            {
+                KeySize = 256,
+                BlockSize = 128,
+                Mode = CipherMode.CBC
+            };
+
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fsOut.Write(buffer, 0, read);
+                }
+                cs.Flush();
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+    }
+}
---
+++ Locker/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Locker
+{
+    enum Mode
+    {
+        encrypt = 0,
+        decrypt = 1
+    }
+
+    class Program
+    {
+        internal static string InputDirectory { get; set; }
+        internal static string OutputDirectory { get; set; }
+        internal static string Password { get; set; }
+        internal static Mode Mode { get; set; }
+
+        static void Main(string[] args)
+        {
+            //ReadArgs(args);
+
+            InputDirectory = @"C:\Users\Eduardo\Downloads\Temp";
+            OutputDirectory = @"C:\Users\Eduardo\Downloads\TempEncrypt";
+            Mode = Mode.encrypt;
+            InputDirectory = @"C:\Users\Eduardo\Downloads\TempEncrypt";
+            OutputDirectory = @"C:\Users\Eduardo\Downloads\TempDecrypt";
+            Mode = Mode.decrypt;
+            Password = "123456";
+
+            if (Mode == Mode.encrypt)
+            {
+                var inputDirectoryInfo = new DirectoryInfo(InputDirectory);
+
+                if (!inputDirectoryInfo.Exists)
+                    throw new DirectoryNotFoundException(InputDirectory);
+
+                if (!Directory.Exists(OutputDirectory))
+                    Directory.CreateDirectory(OutputDirectory);
+
+                FileCipher fc = new FileCipher();
+
+                fc.EncryptFiles(inputDirectoryInfo.GetFiles(), OutputDirectory, Password);
+
+                string hashPassword = fc.Hash(Password);
+
+                var passwordPath = Path.Combine(OutputDirectory, "password");
+
+                File.WriteAllText(passwordPath, hashPassword);
+
+                return;
+            }
+            
+            if (Mode == Mode.decrypt)
+            {
+                var inputDirectoryInfo = new DirectoryInfo(InputDirectory);
+
+                if (!inputDirectoryInfo.Exists)
+                    throw new DirectoryNotFoundException(InputDirectory);
+
+                FileCipher fc = new FileCipher();
+
+                var password = inputDirectoryInfo.GetFiles("password").Single().FullName;
+
+                if (fc.Verify(Password, File.ReadAllText(password)))
+                {
+                    if (!Directory.Exists(OutputDirectory))
+                        Directory.CreateDirectory(OutputDirectory);
+
+                    fc.DecryptFiles(inputDirectoryInfo.GetFiles().Where(x => x.FullName != password), OutputDirectory, password);
+                }
+            }
+        }
+
+        private static void ReadArgs(string[] args)
+        {
+            var arg = string.Empty;
+            
+            Mode = (Mode)Enum.Parse(typeof(Mode), args[0]);
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                arg = args[i];
+
+                switch (arg)
+                {
+                    case "-d":
+                        InputDirectory = args[++i];
+                        break;
+                    case "-p":
+                        Password = args[++i];
+                        break;
+                    case "-o":
+                        OutputDirectory = args[++i];
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
